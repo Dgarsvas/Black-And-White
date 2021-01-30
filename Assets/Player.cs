a0014c7a87261bd8c3a85aaee2339d6d92c0bbd3
pijuskri <pijuskri@gmail.com>
2021-01-30 12:15:20 +0200

    enemy damage, hiding

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,6 +13,10 @@ public class Player : MonoBehaviour
 
     NavMeshAgent agent;
     float speed = 0.5f; //speed of movement
+    public float health = 100;
+    public bool hiding = false;
+    //Quaternion hidingRotation;
+    Vector3 hidingDirection;
 
     void Start()
     {
@@ -32,13 +36,43 @@ public class Player : MonoBehaviour
         //Rotate
         RaycastHit hit = cameraFollow.floorRaycast;
         Vector3 direction = (transform.position - hit.point).normalized;
-        float rot_z = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 180-rot_z, 0f);
+        RotateY(direction);
 
+        //crouch
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (hiding) hiding = false;
+            else Hide();
+        }
+        if (hiding) RotateY(hidingDirection);
+
+        //Shoot
         if (Input.GetMouseButton(0)) currentWeapon.Shoot(cameraFollow.floorRaycast.point);
+
     }
     /// <summary>
-    /// ray cast and detect if hit enemy
+    /// checks if there is a wall and sets player rotation to wall normal
+    /// currently hiding is still active after leaving the wall
+    /// better to be replaced with collision with a field around a wall
     /// </summary>
-    
+    void Hide()
+    {
+        Ray ray = new Ray(transform.position + transform.right/2, transform.right);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 2))
+        {
+            /*
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot = new Vector3(hit.normal);
+            hidingRotation = transform.rotation;
+            hidingRotation = Quaternion.Euler(hidingRotation.eulerAngles.x, hit.normal., hidingRotation.eulerAngles.z);
+            */
+            hidingDirection = -hit.normal;
+            hiding = true;
+        }
+    }
+    void RotateY(Vector3 direction) {
+        float rot_z = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 180 - rot_z, 0f);
+    }
 }
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,54 +5,83 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
 	public string name = "";
+	public float damage = 0;
 	public int ammo = 0;
 	public int ammoCapacity = 0;
 	public int mag = 0;
 	public int magCapacity = 0;
 	public float shootInterval = 0;
 	public float reloadTime = 0;
+	public float bulletSpread = 0;
 
 	float timeSinceShot = 0;
 	float reloadingSinceTime = 0;
 	bool reloading = false;
 
 	AudioSource audioSource;
+	LineRenderer shotRay;
 
 	void Start()
-    {
+	{
+		shotRay = GetComponent<LineRenderer>();
 		audioSource = GetComponent<AudioSource>();
-    }
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 		timeSinceShot += Time.deltaTime;
-		if(reloading) reloadingSinceTime += Time.deltaTime;
+		if (reloading) reloadingSinceTime += Time.deltaTime;
 		if (reloading && reloadingSinceTime > reloadTime) FinishReload();
 
 		if (Input.GetKeyDown(KeyCode.R)) Reload();
-    }
+	}
 
+	/// <summary>
+	/// ray cast and detect if hit enemy
+	/// </summary>
 	public void Shoot(Vector3 point)
 	{
 		if (mag > 0 && timeSinceShot > shootInterval && !reloading)
 		{
 			mag--;
 			timeSinceShot = 0;
-			Debug.Log("shot");
 			audioSource.Play();
 
-			Vector3 direction = (point - transform.position);
-			direction = new Vector3(direction.x, 0, direction.z).normalized;
+			//Vector3 direction = (point - transform.position);
+			//direction = new Vector3(direction.x, 0, direction.z);
+
+			Vector3 direction = transform.right;
+			//apply bullet spread
+			//direction += transform.up * (Random.value*2 - 1) * bulletSpread + transform.forward * (Random.value * 2 - 1) * bulletSpread;
+			//direction = direction.normalized;
+
+
 			Ray ray = new Ray(transform.position + direction, direction);
 			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag.Equals("Enemy"))
+			if (Physics.Raycast(ray, out hit))
 			{
-				Debug.DrawRay(transform.position, direction * 10f, Color.red);
+				if (hit.collider.gameObject.CompareTag("Enemy")) 
+				{
+					Debug.DrawRay(transform.position, direction * 10f, Color.red);
+					Enemy enemy = hit.collider.gameObject.GetComponent<Enemy>();
+					enemy.health -= damage;
+					if(enemy.health<=0) hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(direction * 500f);
+				}
+				else if (hit.collider.gameObject.CompareTag("Player"))
+				{
+
+				}
+				else Debug.DrawRay(transform.position, direction * 10f);
+				
 			}
-			else Debug.DrawRay(transform.position, direction * 10f);
+			//Vector3[] points = { transform.position, hit.point };
+			Vector3[] points = { transform.position, transform.position + direction * (hit.distance + 1) };
+			shotRay.SetPositions(points);
+
 		}
 		if (mag == 0) Reload();
 	}
+
 	public void Reload() {
 		if (ammo > 0 && mag < magCapacity) {
 			reloading = true;
