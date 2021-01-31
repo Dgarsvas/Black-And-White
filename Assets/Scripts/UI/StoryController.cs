830ef0a424a3da993650a4239d3ae348dcb17aac
DomantasGarsvas <dgarsvas@gmail.com>
2021-01-31 15:57:59 +0200

    renamed to main scene, added story scene

---
+++ Assets/Scripts/UI/StoryController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StoryController : MonoBehaviour
+{
+    public Sprite[] storyImages;
+    public Image image;
+
+    bool canSkip;
+
+    Coroutine cor;
+
+    public void Start()
+    {
+        FadeManager.instance.StartFadeIn(() =>
+        {
+            canSkip = true;
+            cor = StartCoroutine(Story());
+        });
+    }
+
+    private IEnumerator Story()
+    {
+        WaitForSeconds seconds = new WaitForSeconds(5f);
+        for (int i = 0; i < storyImages.Length; i++)
+        {
+            Show(storyImages[i]);
+            yield return seconds;
+        }
+        End();
+    }
+
+    private void Show(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    public void Update()
+    {
+        if (canSkip)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                End();
+            }
+        }
+    }
+
+    private void End()
+    {
+        StopCoroutine(cor);
+        FadeManager.instance.StartFadeOut(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+    }
+}
